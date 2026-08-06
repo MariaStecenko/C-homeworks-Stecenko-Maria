abbc15fdf0971ea6f946b6ffbcbf7a7730a0055a
MariaStecenko <stecenkomaria@knu.ua>
2026-08-06 14:34:57 +0300

    Update 7.2.cpp

--- 7.2.cpp
+++ 7.2.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int arr[4] = {5, 112, 4, 3};
+
+    cout << "Елементи масиву навпаки: ";
+    
+    for (int i = 3; i >= 0; i--) {
+        cout << arr[i];
+        if (i > 0) {
+            cout << ", ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
