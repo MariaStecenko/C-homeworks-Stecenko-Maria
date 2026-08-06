79aa9fb9c69e813f2e9455539b6ff8e91541abd2
MariaStecenko <stecenkomaria@knu.ua>
2026-08-06 13:47:42 +0300

    Update 4.8.cpp

--- 4.8.cpp
+++ 4.8.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int m;
+
+    cout << "Ціле число m (m >= 1): ";
+    cin >> m;
+
+    if (m < 1) {
+        cout << "Число m має бути >= 1!" << endl;
+        return 1;
+    }
+
+    int k = 0;
+    long long val = 1; // 4^0 = 1
+
+    while (val * 4 <= m) {
+        val *= 4;
+        k++;
+    }
+
+    cout << "Найбільше k = " << k << " (бо 4^" << k << " = " << val << " <= " << m << ")" << endl;
+
+    return 0;
+}
