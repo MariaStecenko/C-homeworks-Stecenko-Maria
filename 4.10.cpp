cee7eba28678272878e292145bdbb8dc782223e9
MariaStecenko <stecenkomaria@knu.ua>
2026-08-06 13:50:30 +0300

    Update 4.10.cpp

--- 4.10.cpp
+++ 4.10.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <limits>
 
+using namespace std;
+
+int main() {
+    float a = 1.0f;
+    while ((1.0f + a / 2.0f) != 1.0f) {
+        a = a / 2.0f;
+    }
+
+    cout << "Обчислений машинний нуль (eps) = " << a << endl;
+
+    float std_eps = numeric_limits<float>::epsilon();
+    cout << "Значення зі стандартної бібліотеки (std::numeric_limits) = " << std_eps << endl;
+
+    return 0;
+}
