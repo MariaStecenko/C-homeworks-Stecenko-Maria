2fa356e6f8b329b1bc3567bbb9c77520eead6adf
MariaStecenko <stecenkomaria@knu.ua>
2026-08-06 13:48:27 +0300

    Update 4.9.cpp

--- 4.9.cpp
+++ 4.9.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int n;
+
+    cout << "Натуральне число n: ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Число n має бути натуральним (n > 0)!" << endl;
+        return 1;
+    }
+
+    long long powerOfTwo = 1; // 2^0 = 1
+    int r = 0;
+
+    while (powerOfTwo <= n) {
+        powerOfTwo *= 2;
+        r++;
+    }
+
+    cout << "Найменше число вигляду 2^r, яке більше за " << n << ": " << powerOfTwo << endl;
+    cout << "r = " << r << endl;
+
+    return 0;
+}
