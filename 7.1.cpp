886d28be015ee73fa4902434fc64e26939768a6d
MariaStecenko <stecenkomaria@knu.ua>
2026-08-06 14:34:32 +0300

    Update 7.1.cpp

--- 7.1.cpp
+++ 7.1.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int arr[5] = {10, 3, 7, 1, 5};
+    
+    double x;
+    cout << "Масив: ";
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Дійсне число: ";
+    cin >> x;
+
+    int count = 0;
+    for (int i = 0; i < 5; i++) {
+        if (arr[i] < x) {
+            count++;
+        }
+    }
+
+    cout << "Кількість елементів, менших за " << x << ": " << count << endl;
+
+    return 0;
+}
