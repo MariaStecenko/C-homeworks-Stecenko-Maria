46206f0539933e9767e8bd97a35fb1508f026153
MariaStecenko <stecenkomaria@knu.ua>
2026-08-06 14:37:19 +0300

    Update 7.4.cpp

--- 7.4.cpp
+++ 7.4.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
 
+using namespace std;
+
+void inputArray(int arr[], int size) {
+    cout << "Введіть " << size << " цілих чисел:" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << "arr[" << i << "] = ";
+        cin >> arr[i];
+    }
+}
+
+int findMax(int arr[], int size) {
+    int maxVal = arr[0];
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > maxVal) {
+            maxVal = arr[i];
+        }
+    }
+    return maxVal;
+}
+
+int main() {
+    const int SIZE = 5;
+    int numbers[SIZE];
+
+    inputArray(numbers, SIZE);
+
+    int maxNumber = findMax(numbers, SIZE);
+    cout << "\nМаксимальне значення в масиві: " << maxNumber << endl;
+
+    return 0;
+}
