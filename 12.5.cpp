89ac0902d5b1a1b5487b877b78cda864cafdd804
MariaStecenko <stecenkomaria@knu.ua>
2026-08-08 16:41:16 +0300

    Update 12.5.cpp

--- 12.5.cpp
+++ 12.5.cpp
@@ -1,1 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <iomanip>
 
+using namespace std;
+
+int main() {
+    ifstream inFile("input.txt");
+
+    if (!inFile.is_open()) {
+        cout << "Помилка: не вдалося відкрити файл input.txt!" << endl;
+        return 1;
+    }
+
+    vector<unsigned long long> numbers;
+    unsigned long long val;
+
+    while (inFile >> val) {
+        numbers.push_back(val);
+    }
+
+    inFile.close();
+
+    if (numbers.empty()) {
+        cout << "Файл порожній або не містить коректних чисел!" << endl;
+        return 0;
+    }
+
+    for (int i = numbers.size() - 1; i >= 0; i--) {
+        double root = sqrt(numbers[i]);
+        cout << fixed << setprecision(3) << root << endl;
+    }
+
+    return 0;
+}
