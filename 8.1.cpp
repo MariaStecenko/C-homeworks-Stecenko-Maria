268eb9678eb9fb0a901dbe38870ce961f18f7363
MariaStecenko <stecenkomaria@knu.ua>
2026-08-08 15:41:19 +0300

    Update 8.1.cpp

--- 8.1.cpp
+++ 8.1.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
 
+using namespace std;
+
+int main() {
+    int matrix[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int N, M;
+    cout << "Введіть число M (яке шукаємо для заміни): ";
+    cin >> M;
+    cout << "Введіть число N (на яке замінюємо): ";
+    cin >> N;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (matrix[i][j] == M) {
+                matrix[i][j] = N;
+            }
+        }
+    }
+
+    cout << "\nОтримана матриця:" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
