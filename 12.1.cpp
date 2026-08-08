6ca84a7a9afaf1e6506eade8d6783371a386a211
MariaStecenko <stecenkomaria@knu.ua>
2026-08-08 16:33:35 +0300

    Update 12.1.cpp

--- 12.1.cpp
+++ 12.1.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
 
+using namespace std;
+
+int main() {
+    double x, y;
+
+    cout << "Введіть число x: ";
+    cin >> x;
+    cout << "Введіть число y: ";
+    cin >> y;
+
+    double result = pow(x, y);
+
+    cout << "\nДесяткове представлення: " << fixed << result << endl;
+    cout << "Наукове представлення:  " << scientific << result << endl;
+
+    return 0;
+}
